63472cc7e83a991d8bb1d8a1f6653b5f25f48e7b
Adam H. Peterson <adpeters@adobe.com>
2022-11-03 13:16:09 -0600

    Change sort to remove the blank flag.

--- toy/command.sh
+++ toy/command.sh
@@ -2,19 +2,19 @@ filename=toy.txt
 join -t $'\t' \
     <(tr $'\t' $'\n' < toy_schema.txt |
       cat -n |
-      sort -k 1b,1) \
+      sort -k 1,1) \
     <(join -t $'\t' \
         <(join -t $'\t' \
             <(cat -n "$filename" |
               cut -f 1,7 |
               grep -e $'\tgamma' |
-              sort -k 1b,1) \
+              sort -k 1,1) \
             <(cat -n "$filename" |
               cut -f 1,6 |
               grep -e $'\tbaz' |
-              sort -k 1b,1) |
+              sort -k 1,1) |
             cut -f 1) \
-        <(cat -n "$filename" | sort -k 1b,1) |
+        <(cat -n "$filename" | sort -k 1,1) |
       cut -f 2- |
       head -n1 |
       tr $'\t' $'\n' |
